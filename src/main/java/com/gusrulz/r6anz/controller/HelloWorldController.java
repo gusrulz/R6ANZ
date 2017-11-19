78dc1212756fc952823d7501d2e9035c75f705d9
gusrulz <thomas.k.martin@hotmail.com.au>
2017-11-20 00:11:07 +1100

    T.M. - Fixed web application set up.

--- src/main/java/com/gusrulz/r6anz/controller/HelloWorldController.java
+++ src/main/java/com/gusrulz/r6anz/controller/HelloWorldController.java
@@ -2,22 +2,15 @@ package com.gusrulz.r6anz.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
  
 @Controller
 public class HelloWorldController {
-	String message = "Welcome to Spring MVC!";
- 
-	@RequestMapping("/hello")
-	public ModelAndView showMessage(
-			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-		System.out.println("in controller");
- 
-		ModelAndView mv = new ModelAndView("helloworld");
-		mv.addObject("message", message);
-		mv.addObject("name", name);
-		return mv;
+	@GetMapping("/hello")
+	public String hello(Model model) {
+
+		model.addAttribute("name", "John Doe");
+
+		return "welcome";
 	}
 }
